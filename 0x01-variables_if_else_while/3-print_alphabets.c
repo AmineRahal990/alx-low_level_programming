75f63310619dc517b0b0b725522a840adb404d71
aminerahal990 <aminegit@gmail.com>
2023-06-16 18:14:13 -0700

    f

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase then uppercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        int i;
+        char low[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for (i = 0; i < 52; i++)
+        {
+                putchar(low[i]);
+        }
+putchar('\n');
+return (0);
+}
